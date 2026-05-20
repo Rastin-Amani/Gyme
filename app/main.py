2b4b391db25c01ee682fb85b34b6c4409a71573e
Rastin-Amani <103758251+Rastin-Amani@users.noreply.github.com>
2026-05-20 14:15:08 +0330

    added LOCAL swagger

--- app/main.py
+++ app/main.py
@@ -1,17 +1,49 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from .routes import dashboard
 
-from .routes import dashboard, clients
-
-app = FastAPI()
-
+###disable default swagger ui
+app = FastAPI(
+    title="Gyme",
+    docs_url=None,
+    redoc_url=None,
+    openapi_url="/openapi.json",
+)
 # static folder
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# template engine
-templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # include routers
 app.include_router(dashboard.router)
-app.include_router(clients.router)
+
+@app.get("/docs", include_in_schema=False)
+def custom_docs():
+    return HTMLResponse("""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Gyme API Docs</title>
+        <link rel="stylesheet" type="text/css" href="/static/swagger/swagger-ui.css">
+    </head>
+    <body>
+        <div id="swagger-ui"></div>
+
+        <script src="/static/swagger/swagger-ui-bundle.js"></script>
+        <script src="/static/swagger/swagger-ui-standalone-preset.js"></script>
+        <script>
+        window.onload = function() {
+            SwaggerUIBundle({
+                url: '/openapi.json',
+                dom_id: '#swagger-ui',
+                presets: [
+                    SwaggerUIBundle.presets.apis,
+                    SwaggerUIStandalonePreset
+                ],
+                layout: "StandaloneLayout"
+            });
+        };
+        </script>
+    </body>
+    </html>
+    """)
