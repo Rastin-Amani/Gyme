2c1af268348cdb6c7250de646de9d10237ddb2b0
Rastin Amani <rastin@vm34956-55655.mobinhost.com>
2026-08-02 14:21:07 +0000

    chore: update PocketBase dev URL

--- app/pb.py
+++ app/pb.py
@@ -2,7 +2,7 @@ import os
 from pocketbase import PocketBase
 
 # Fetches PB_URL from the environment, defaults to local if not set
-PB_URL = os.getenv("PB_URL", "http://127.0.0.1:8091")
+PB_URL = os.getenv("PB_URL", "http://db.dev.gyme.cloud")
 pb = PocketBase(PB_URL)
 
 def get_pb():
